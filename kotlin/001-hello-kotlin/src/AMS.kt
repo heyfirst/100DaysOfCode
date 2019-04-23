f7e0f29769b31bf7569fc5431882e321643de20c
Kanisorn S <firstziiz.k@gmail.com>
2019-04-23 23:51:25 +0700

    [ADD] shouldChangeWater

--- kotlin/001-hello-kotlin/src/AMS.kt
+++ kotlin/001-hello-kotlin/src/AMS.kt
@@ -7,8 +7,25 @@ fun main(args: Array<String>) {
 fun feedTheFish() {
     val day = randomDay()
     val food = fishFood(day)
+    println("Today is $day and the first eat $food.")
 
-    println("Today is $day and the first eat $food")
+    if(shouldChangeWater(day)) {
+        println("Change the water today.")
+    }
+
+    swim(50, speed="slow")
+}
+
+fun swim(time: Int, speed: String = "fast") {
+    println("swimming $speed")
+}
+
+fun shouldChangeWater(
+    day: String,
+    temperature: Int = 22,
+    dirty: Int = 20
+) : Boolean {
+    return true
 }
 
 fun fishFood(day : String) : String {
@@ -27,4 +44,14 @@ fun fishFood(day : String) : String {
 fun randomDay() : String {
     val weeks = listOf("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
     return weeks[Random().nextInt(7)]
+}
+
+
+fun canAddFish (
+    tankSize: Double,
+    currentFish: List<Int>,
+    fishSize: Int = 2,
+    hasDecorations: Boolean = true
+): Boolean {
+    return tankSize * if (hasDecorations) 0.8 else 1.0 >= (currentFish.sum() + fishSize)
 }
