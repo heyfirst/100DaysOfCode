e29103ec238234caffab6684b7a339d657f72635
Kanisorn S <firstziiz.k@gmail.com>
2019-06-16 19:37:09 +0700

    Finish Django Template 101

--- learning-django-template/learning_django_template/events/views.py
+++ learning-django-template/learning_django_template/events/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+# some_app/views.py
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class ListOfEvents(TemplateView):
+    template_name = "events/events.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['events'] = [
+            {
+                'title': 'Code In The Dark',
+                'date': '10 Oct 2020'
+            },
+            {
+                'title': 'Codemania',
+                'date': '15 Oct 2020'
+            },
+            {
+                'title': 'Pycon SG',
+                'date': '20 Oct 2020'
+            },
+        ]
+        context['user'] = {
+            'name': 'Kanisorn Sutham',
+        }
+
+        return context
