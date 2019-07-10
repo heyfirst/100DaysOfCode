d67b89638d9ccd9d5c56137d4d5bffb0ea31ae39
Kanisorn Sutham <firstziiz.k@gmail.com>
2019-07-10 19:07:21 +0800

    add feature flag in java

---
+++ feature-flag/src/main/java/com/tw/featureflag/FeatureFlagApplication.java
@@ -0,0 +1,13 @@
+package com.tw.featureflag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureFlagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureFlagApplication.class, args);
+	}
+
+}
